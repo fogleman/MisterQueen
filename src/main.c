4d4160b7fd8519f7020b4b2167664ee7ad11848d
Michael Fogleman <fogleman@gmail.com>
2014-10-17 23:17:34 -0400

    sort_moves ftw, mate fixes

--- src/main.c
+++ src/main.c
@@ -2,17 +2,13 @@
 #include "bb.h"
 #include "board.h"
 #include "move.h"
-#include "perft.h"
 #include "search.h"
 
 int main(int argc, char **argv) {
     bb_init();
-    // perft_tests();
     Board board;
     Move move;
     board_reset(&board);
-    // board_load_fen(&board, "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -");
-    // board_load_fen(&board, "rn2kb1r/pp1q2p1/2p1pn2/3p3p/3P1N2/2N2P2/PPP3PP/R1BQKB1R w KQkq - 4 10");
     while (1) {
         board_print(&board);
         while (1) {
--- src/search.c
+++ src/search.c
@@ -9,6 +9,31 @@
 
 static Table TABLE;
 
+void sort_moves(Board *board, Move *moves, int count, int *indexes) {
+    int scores[MAX_MOVES];
+    Undo undo;
+    for (int i = 0; i < count; i++) {
+        Move *move = moves + i;
+        do_move(board, move, &undo);
+        scores[i] = evaluate(board);
+        indexes[i] = i;
+        undo_move(board, move, &undo);
+    }
+    for (int i = 1; i < count; i++) {
+        int j = i;
+        while (j > 0 && scores[j - 1] > scores[j]) {
+            int temp;
+            temp = scores[j];
+            scores[j] = scores[j - 1];
+            scores[j - 1] = temp;
+            temp = indexes[j];
+            indexes[j] = indexes[j - 1];
+            indexes[j - 1] = temp;
+            j--;
+        }
+    }
+}
+
 int quiesce(Board *board, int alpha, int beta) {
     int score = evaluate(board);
     if (score >= beta) {
@@ -19,9 +44,11 @@ int quiesce(Board *board, int alpha, int beta) {
     }
     Undo undo;
     Move moves[MAX_MOVES];
+    int indexes[MAX_MOVES];
     int count = gen_attacks(board, moves);
+    sort_moves(board, moves, count, indexes);
     for (int i = 0; i < count; i++) {
-        Move *move = &moves[i];
+        Move *move = &moves[indexes[i]];
         int p1 = PIECE(board->squares[move->src]);
         int p2 = PIECE(board->squares[move->dst]);
         if (p2 < p1) {
@@ -42,14 +69,13 @@ int quiesce(Board *board, int alpha, int beta) {
 
 int alpha_beta(Board *board, int depth, int alpha, int beta) {
     if (is_illegal(board)) {
-        return board->color ? -INF : INF;
+        return INF;
     }
     Entry *entry = table_get(&TABLE, board->hash);
     if (entry->key == board->hash && entry->depth == depth) {
         return entry->value;
     }
     if (depth <= 0) {
-        // return evaluate(board);
         return quiesce(board, alpha, beta);
     }
     do_null_move(board);
@@ -60,17 +86,31 @@ int alpha_beta(Board *board, int depth, int alpha, int beta) {
     }
     Undo undo;
     Move moves[MAX_MOVES];
+    int indexes[MAX_MOVES];
     int count = gen_moves(board, moves);
+    sort_moves(board, moves, count, indexes);
+    int legal = 0;
     for (int i = 0; i < count; i++) {
-        Move *move = &moves[i];
+        Move *move = &moves[indexes[i]];
         do_move(board, move, &undo);
         int score = -alpha_beta(board, depth - 1, -beta, -alpha);
         undo_move(board, move, &undo);
+        if (score > -INF) {
+            legal = 1;
+        }
         if (score >= beta) {
             return beta;
         }
         if (score > alpha) {
             alpha = score;
+        }
+    }
+    if (!legal) {
+        if (is_check(board)) {
+            return -INF + 1;
+        }
+        else {
+            return 0;
         }
     }
     table_set(&TABLE, board->hash, depth, alpha, NULL);
@@ -102,24 +142,17 @@ int root_search(Board *board, int depth, int alpha, int beta, Move *result) {
 void search(Board *board, double duration, Move *move) {
     table_alloc(&TABLE, 22);
     double start = now();
-    int alpha = -INF;
-    int beta = INF;
-    int window = 100;
     int depth = 1;
     while (1) {
-        int score = root_search(board, depth, alpha, beta, move);
-        if (score <= alpha || score >= beta) {
-            alpha = -INF;
-            beta = INF;
-            continue;
-        }
+        int score = root_search(board, depth, -INF, INF, move);
         printf("%d, %d, ", depth, score);
         print_move(board, move);
         if (now() - start > duration) {
             break;
         }
-        // alpha = score - window;
-        // beta = score + window;
+        if (score == INF - 1 || score == -INF + 1) {
+            break;
+        }
         depth++;
     }
     table_free(&TABLE);
