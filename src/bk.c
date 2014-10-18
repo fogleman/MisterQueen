ecfb463a64aed1d89182aff959500693d4d0a29f
Michael Fogleman <fogleman@gmail.com>
2014-10-18 11:03:10 -0400

    more test cases

--- src/bk.c
+++ src/bk.c
@@ -15,6 +15,71 @@ void bk_test(char *fen, char *bm) {
 
 void bk_tests() {
     char *data[] = {
+        // https://chessprogramming.wikispaces.com/LCT+II
+        "r3kb1r/3n1pp1/p6p/2pPp2q/Pp2N3/3B2PP/1PQ2P2/R3K2R w KQkq -", "d6",
+        "1k1r3r/pp2qpp1/3b1n1p/3pNQ2/2pP1P2/2N1P3/PP4PP/1K1RR3 b - -", "Bb4",
+        "r6k/pp4p1/2p1b3/3pP3/7q/P2B3r/1PP2Q1P/2K1R1R1 w - -", "Qc5",
+        "1nr5/2rbkppp/p3p3/Np6/2PRPP2/8/PKP1B1PP/3R4 b - -", "e5",
+        "2r2rk1/1p1bq3/p3p2p/3pPpp1/1P1Q4/P7/2P2PPP/2R1RBK1 b - -", "Bb5",
+        "3r1bk1/p4ppp/Qp2p3/8/1P1B4/Pq2P1P1/2r2P1P/R3R1K1 b - -", "e5",
+        "r1b2r1k/pp2q1pp/2p2p2/2p1n2N/4P3/1PNP2QP/1PP2RP1/5RK1 w - -", "Nd1",
+        "r2qrnk1/pp3ppb/3b1n1p/1Pp1p3/2P1P2N/P5P1/1B1NQPBP/R4RK1 w - -", "Bh3",
+        "5nk1/Q4bpp/5p2/8/P1n1PN2/q4P2/6PP/1R4K1 w - -", "Qd4",
+        "r3k2r/3bbp1p/p1nppp2/5P2/1p1NP3/5NP1/PPPK3P/3R1B1R b kq -", "Bf8",
+        "bn6/1q4n1/1p1p1kp1/2pPp1pp/1PP1P1P1/3N1P1P/4B1K1/2Q2N2 w - -", "h4",
+        "3r2k1/pp2npp1/2rqp2p/8/3PQ3/1BR3P1/PP3P1P/3R2K1 b - -", "Rb6",
+        "1r2r1k1/4ppbp/B5p1/3P4/pp1qPB2/2n2Q1P/P4PP1/4RRK1 b - -", "Nxa2",
+        "r2qkb1r/1b3ppp/p3pn2/1p6/1n1P4/1BN2N2/PP2QPPP/R1BR2K1 w kq -", "d5",
+        "1r4k1/1q2bp2/3p2p1/2pP4/p1N4R/2P2QP1/1P3PK1/8 w - -", "Nxd6",
+        "rn3rk1/pbppq1pp/1p2pb2/4N2Q/3PN3/3B4/PPP2PPP/R3K2R w KQ -", "Qxh7+",
+        "4r1k1/3b1p2/5qp1/1BPpn2p/7n/r3P1N1/2Q1RPPP/1R3NK1 b - -", "Qf3",
+        "2k2b1r/1pq3p1/2p1pp2/p1n1PnNp/2P2B2/2N4P/PP2QPP1/3R2K1 w - -", "exf6",
+        "2r2r2/3qbpkp/p3n1p1/2ppP3/6Q1/1P1B3R/PBP3PP/5R1K w - -", "Rxh7+",
+        "2r1k2r/2pn1pp1/1p3n1p/p3PP2/4q2B/P1P5/2Q1N1PP/R4RK1 w q -", "exf6",
+        "2rr2k1/1b3ppp/pb2p3/1p2P3/1P2BPnq/P1N3P1/1B2Q2P/R4R1K b - -", "Rxc3",
+        "2b1r1k1/r4ppp/p7/2pNP3/4Q3/q6P/2P2PP1/3RR1K1 w - -", "Nf6+",
+        "6k1/5p2/3P2p1/7n/3QPP2/7q/r2N3P/6RK b - -", "Rxd2",
+        "rq2rbk1/6p1/p2p2Pp/1p1Rn3/4PB2/6Q1/PPP1B3/2K3R1 w - -", "Bxh6",
+        "rnbq2k1/p1r2p1p/1p1p1Pp1/1BpPn1N1/P7/2P5/6PP/R1B1QRK1 w - -", "Nxh7",
+        "r2qrb1k/1p1b2p1/p2ppn1p/8/3NP3/1BN5/PPP3QP/1K3RR1 w - -", "e5",
+        "8/1p3pp1/7p/5P1P/2k3P1/8/2K2P2/8 w - -", "f6",
+        "8/pp2r1k1/2p1p3/3pP2p/1P1P1P1P/P5KR/8/8 w - -", "f5",
+        "8/3p4/p1bk3p/Pp6/1Kp1PpPp/2P2P1P/2P5/5B2 b - -", "Bxe4",
+        "5k2/7R/4P2p/5K2/p1r2P1p/8/8/8 b - -", "h3; am h5",
+        "6k1/6p1/7p/P1N5/1r3p2/7P/1b3PP1/3bR1K1 w - -", "a6",
+        "8/3b4/5k2/2pPnp2/1pP4N/pP1B2P1/P3K3/8 b - -", "f4",
+        "6k1/4pp1p/3p2p1/P1pPb3/R7/1r2P1PP/3B1P2/6K1 w - -", "Bb4",
+        "2k5/p7/Pp1p1b2/1P1P1p2/2P2P1p/3K3P/5B2/8 w - -", "c5",
+        "8/5Bp1/4P3/6pP/1b1k1P2/5K2/8/8 w - -", "Kg4",
+
+        // https://chessprogramming.wikispaces.com/Kaufman+Test
+        "1rbq1rk1/p1b1nppp/1p2p3/8/1B1pN3/P2B4/1P3PPP/2RQ1R1K w - -", "Nf6+",
+        "3r2k1/p2r1p1p/1p2p1p1/q4n2/3P4/PQ5P/1P1RNPP1/3R2K1 b - -", "Nxd4",
+        "3r2k1/1p3ppp/2pq4/p1n5/P6P/1P6/1PB2QP1/1K2R3 w - -", "Rd1",
+        "r1b1r1k1/1ppn1p1p/3pnqp1/8/p1P1P3/5P2/PbNQNBPP/1R2RB1K w - -", "Rxb2",
+        "2r4k/pB4bp/1p4p1/6q1/1P1n4/2N5/P4PPP/2R1Q1K1 b - -", "Qxc1",
+        "r5k1/3n1ppp/1p6/3p1p2/3P1B2/r3P2P/PR3PP1/2R3K1 b - -", "Rxa2",
+        "2r2rk1/1bqnbpp1/1p1ppn1p/pP6/N1P1P3/P2B1N1P/1B2QPP1/R2R2K1 b - -", "Bxe4",
+        "5r1k/6pp/1n2Q3/4p3/8/7P/PP4PK/R1B1q3 b - -", "h6",
+        "r3k2r/pbn2ppp/8/1P1pP3/P1qP4/5B2/3Q1PPP/R3K2R w KQkq -", "Be2",
+        "3r2k1/ppq2pp1/4p2p/3n3P/3N2P1/2P5/PP2QP2/K2R4 b - -", "Nxc3",
+        "q3rn1k/2QR4/pp2pp2/8/P1P5/1P4N1/6n1/6K1 w - -", "Nf5",
+        "6k1/p3q2p/1nr3pB/8/3Q1P2/6P1/PP5P/3R2K1 b - -", "Rd6",
+        "1r4k1/7p/5np1/3p3n/8/2NB4/7P/3N1RK1 w - -", "Nxd5",
+        "1r2r1k1/p4p1p/6pB/q7/8/3Q2P1/PbP2PKP/1R3R2 w - -", "Rxb2",
+        "r2q1r1k/pb3p1p/2n1p2Q/5p2/8/3B2N1/PP3PPP/R3R1K1 w - -", "Bxf5",
+        "8/4p3/p2p4/2pP4/2P1P3/1P4k1/1P1K4/8 w - -", "b4",
+        "1r1q1rk1/p1p2pbp/2pp1np1/6B1/4P3/2NQ4/PPP2PPP/3R1RK1 w - -", "e5",
+        "q4rk1/1n1Qbppp/2p5/1p2p3/1P2P3/2P4P/6P1/2B1NRK1 b - -", "Qc8",
+        "r2q1r1k/1b1nN2p/pp3pp1/8/Q7/PP5P/1BP2RPN/7K w - -", "Qxd7",
+        "8/5p2/pk2p3/4P2p/2b1pP1P/P3P2B/8/7K w - -", "Bg4",
+        "8/2k5/4p3/1nb2p2/2K5/8/6B1/8 w - -", "Kxb5",
+        "1B1b4/7K/1p6/1k6/8/8/8/8 w - -", "Ba7",
+        "rn1q1rk1/1b2bppp/1pn1p3/p2pP3/3P4/P2BBN1P/1P1N1PP1/R2Q1RK1 b - -", "Ba6",
+        "8/p1ppk1p1/2n2p2/8/4B3/2P1KPP1/1P5P/8 w - -", "Bxc6",
+        "8/3nk3/3pp3/1B6/8/3PPP2/4K3/8 w - -", "Bxd7",
+
+        // https://chessprogramming.wikispaces.com/Bratko-Kopec+Test
         "1k1r4/pp1b1R2/3q2pp/4p3/2B5/4Q3/PPP2B2/2K5 b - -", "Qd1+",
         "3r1k2/4npp1/1ppr3p/p6P/P2PPPP1/1NR5/5K2/2R5 w - -", "d5",
         "2q1rr1k/3bbnnp/p2p1pp1/2pPp3/PpP1P1P1/1P2BNNP/2BQ1PRK/7R b - -", "f5",
@@ -40,7 +105,8 @@ void bk_tests() {
         "r1bqk2r/pp2bppp/2p5/3pP3/P2Q1P2/2N1B3/1PP3PP/R4RK1 b kq -", "f6",
         "r2qnrnk/p2b2b1/1p1p2pp/2pPpp2/1PP1P3/PRNBB3/3QNPPP/5RK1 w - -", "f4",
     };
-    for (int i = 0; i < 24; i++) {
+    int count = sizeof(data) / sizeof(char *) / 2;
+    for (int i = 0; i < count; i++) {
         char *fen = data[i * 2];
         char *bm = data[i * 2 + 1];
         bk_test(fen, bm);
